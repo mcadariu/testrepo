039684e96a3deef2834e6e0ec50e4ed8e5168a2a
mcadariu <cadariu.mircea@gmail.com>
2017-02-20 11:22:34 +0100

    Added SecondFile.java

---
+++ gigi/src/main/java/com/learnakka/SecondFile.java
@@ -0,0 +1,12 @@
+package com.learnakka;
+
+public class SecondFile {
+
+    public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+        third = first + second;
+        return third;
+    }
+
+}
