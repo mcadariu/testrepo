86c103b17ac79fb1f209d7e7260871c915e61157
mcadariu <cadariu.mircea@gmail.com>
2017-02-27 14:48:37 +0100

    Update HelloWorld.java

--- gigi/src/main/java/com/learnakka/HelloWorld.java
+++ gigi/src/main/java/com/learnakka/HelloWorld.java
@@ -7,29 +7,6 @@ public class HelloWorld {
         third = first + second;
          third = first + second;
          third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
-         third = first + second;
                 return third;
     }
 
