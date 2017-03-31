59c24e172b017947af22ed33ee517b84d276845c
mcadariu <cadariu.mircea@gmail.com>
2017-03-31 11:04:54 +0200

    Update HelloWorld.java

--- gigi/src/main/java/com/learnakka/HelloWorld.java
+++ gigi/src/main/java/com/learnakka/HelloWorld.java
@@ -2,6 +2,299 @@ package com.learnakka;
 
 public class HelloWorld {
 
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
+        public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+         third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add that to this
+
+    }
+    
     public int add(int first, int second) {
         int third = first + second;
         third = first + second;
