41d39f3b8db948d4c905d6e15f46b0fcea4fbb36
mcadariu <cadariu.mircea@gmail.com>
2017-03-31 16:32:27 +0200

    Create Third.java

---
+++ gigi/src/main/java/com/learnakka/Third.java
@@ -0,0 +1,19 @@
+package com.learnakka;
+
+public class Third {
+
+    public int add(int first, int second) {
+        int third = first + second;
+        third = first + second;
+        third = first + second;
+         third = first + second;
+        //TODO add this to that
+        //TODO add this to that
+         //TODO add this to that
+        //TODO add this to that
+         //TODO add this to that
+        //TODO add this to that
+        return third;
+    }
+
+}
