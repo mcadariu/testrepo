0ad0dd8b5a27353208d72d76c30850538fa340b9
mcadariu <cadariu.mircea@gmail.com>
2017-10-05 11:30:41 +0200

    Update HelloWorld.java

--- gigi/src/main/java/com/learnakka/HelloWorld.java
+++ gigi/src/main/java/com/learnakka/HelloWorld.java
@@ -4,36 +4,7 @@ public class HelloWorld {
 
         public int add(int first) {
         int third = first + second;
-        aaaa = aaaaa + aaaa;
-                        aaaa = aaaaa + aaaa;
-                                        aaaa = aaaaa + aaaa;
-                                                        aaaa = aaaaa + aaaa;
-                                                        aaaa = aaaaa + aaaa;
-                                                                        aaaa = aaaaa + aaaa;
-                                                                        aaaa = aaaaa + aaaa;
-         aaaa = aaaaa + aaaa;
-         aaaa = aaaaa + aaaa;
-                
-                   aaaa = aaaaa + aaaa;
-         aaaa = aaaaa + aaaa;
-                
-                   aaaa = aaaaa + aaaa;
-         aaaa = aaaaa + aaaa;
-                
-                   aaaa = aaaaa + aaaa;
-         aaaa = aaaaa + aaaa;
-                
-                   aaaa = aaaaa + aaaa;
-         aaaa = aaaaa + aaaa;
-                
-                   aaaa = aaaaa + aaaa;
-         aaaa = aaaaa + aaaa;
-                
-                   aaaa = aaaaa + aaaa;
-         aaaa = aaaaa + aaaa;
-                
-                   aaaa = aaaaa + aaaa;
-         aaaa = aaaaa + aaaa;
+ 
      
         //TODO add this to that
         //TODO add that to this
